aca8ef464caa177270a0347229babf08c3d3dcf2
Rich Friendlich <rfriend@yahoo.com>
2024-03-06 11:06:28 -0500

    Section 12 Complete. Connecting a Backend & Sending HTTP Requests [Shopping List App]

--- my_code/shopping_list/lib/widgets/grocery_list.dart
+++ my_code/shopping_list/lib/widgets/grocery_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-// import 'package:shopping_list/data/dummy_items.dart';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list/data/categories.dart';
 
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
@@ -12,26 +15,101 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'red-parity-344123-default-rtdb.firebaseio.com', 'shopping_list.json');
+
+    try {
+      final response = await http.get(url);
+
+      // setState(() {
+      //   if (response.statusCode >= 400) {
+      //     _error =
+      //         'Can not connect to database. ${response.statusCode.toString()}';
+      //   }
+      // });
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])
+            .value;
+        loadedItems.add(GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category));
+      }
+      setState(() {
+        _groceryItems = loadedItems;
+        _isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        _error = 'Something went wrong.';
+      });
+    }
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
         MaterialPageRoute(builder: ((context) => const NewItem())));
+
     if (newItem == null) {
       return;
     }
+
     setState(() => _groceryItems.add(newItem));
   }
 
-  void _removeItem(GroceryItem item) {
-    setState(() {
-      _groceryItems.remove(item);
-    });
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
+    setState(() => _groceryItems.remove(item));
+    final url = Uri.https('red-parity-344123-default-rtdb.firebaseio.com',
+        'shopping_list/${item.id}.json');
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+
+        final snackBar = SnackBar(
+          duration: const Duration(milliseconds: 1000),
+          content: Text(
+              'Can not connect to database. ${response.statusCode.toString()}'),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(child: Text('Yo list is Empty'));
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
@@ -52,6 +130,9 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );
+    }
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
